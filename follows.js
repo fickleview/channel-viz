d77898efef47d9aefa4617fdba252a3468054a67
fickleview <gmatthews@fickleview.com>
2014-02-06 04:23:49 -0800

    Update follows.js

--- follows.js
+++ follows.js
@@ -154,16 +154,15 @@
 										width: 600,
 										height: 200,
 										renderer: 'area',
-											if (datastream.id == firstID ) {
-										min: firstMin,
-										max: firstMax,
-										} else {
+										if (datastream.id == firstID ) {
+										  min: firstMin,
+										  max: firstMax,
+									  	 } else {
 										
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										}
-										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
-										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
+									
 										padding: {
 											top: 0.02,
 											right: 0.02,
