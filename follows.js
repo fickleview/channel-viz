eea8fc81bfd21f1dddfdb4358617873e09540da3
fickleview <gmatthews@fickleview.com>
2016-01-26 10:30:23 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -9,7 +9,8 @@
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0;
+		hideForm		= 0,
+		rendererType = 'area';
 	*/
 
 	var defaultKey		= 'BK42UGOoxyUzbrLlbnwMvr4PyhynOc3jeek50iPUome71tQR', // Unique master Xively API key to be used as a default
@@ -19,8 +20,8 @@
 		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
 		dataColor		= 'FFCC99', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1, // To hide input form use value of 1, otherwise set to 0
-		timeZoneOffsetMs        = 0, // -10800,  // Seconds
-		rendererType = 'area'
+		timeZoneOffsetMs        = 0; // -10800,  // Seconds
+		
 		
 // Function Declarations
 
