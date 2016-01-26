ffd0d4bc2941b00019f5b0d3d82c909335c31d14
fickleview <gmatthews@fickleview.com>
2016-01-26 11:00:58 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -185,7 +185,7 @@
 										element: document.querySelector('#graph-' + feedId + '-' + datastream.id),
 										width: 600,
 										height: 200,
-										render: 'line',
+										render: 'bar',
 									
 										min: minSet,
 										max: maxSet,
